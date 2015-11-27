3d26613ad4cb20e3ed1f8894c826f506e304f025
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2015-11-27 11:34:07 +0200

    submit

--- main.php
+++ main.php
@@ -1,5 +1,72 @@
 <?php require_once("header.php"); ?>
 
-<h1>Hello, world!</h1>
+<!-- MENÜÜ -->
+
+<nav class="navbar navbar-inverse navbar-fixed-top">  <!-- default - hall; navbar fixed top hoiab seda üleval kinni -->
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Koidu</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<br></br>
+<br></br>
+
+<!-- SISU -->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-1 col-sm-8">
+			<div class="jumbotron">
+				<h1>Hello, world!</h1>
+				<p>...</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+			</div>
+		</div>
+		<div class="col-md-3 col-md-offset-1 col-sm-4">
+			<h3>Login</h3>
+			<form>
+			  <div class="form-group">
+				
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			  </div>
+			  <div class="form-group">
+				
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			  </div>
+			  <div class="form-group">
+				
+			  </div>
+			  <button type="submit" class="btn btn-success pull-right hidden-xs">Submit</button>
+			  <button type="submit" class="btn btn-success btn-block visible-xs">Submit</button>
+			</form>
+		</div>
+	</div>
+
+
+</div>
 
 <?php require_once("footer.php"); ?>
+
+
+
+
+
+
+
